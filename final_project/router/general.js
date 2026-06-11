c8868d79c5c5c15945c0a83de7d2fb8977844bb1
Suncony <sunconystudios@gmail.com>
2026-06-11 07:40:50 -0400

    change general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,40 +35,109 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     //Write your code here
-    return res.send(JSON.stringify(books, null, 4));
+    const getBooks = new Promise((resolve, reject) => {
+        resolve(books);
+    });
+
+    getBooks
+        .then((bookList) => {
+            return res.send(JSON.stringify(bookList, null, 4));
+        })
+        .catch((err) => {
+            return res.status(500).send(err);
+        });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     //Write your code here
     const isbn = req.params.isbn;
-    return res.send(books[isbn]);
+
+    const getBook = new Promise((resolve, reject) => {
+
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject("Book not found");
+        }
+
+    });
+
+    getBook
+        .then((book) => {
+            return res.send(book);
+        })
+        .catch((err) => {
+            return res.status(404).send(err);
+        });
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     //Write your code here
-    const keys = Object.keys(books);
     const author = req.params.author;
 
-    for (let key of keys) {
-        if (books[key].author === author) {
-            return res.send(books[key]);
+    const getBook = new Promise((resolve, reject) => {
+
+        const keys = Object.keys(books);
+        const matchingBooks = [];
+
+        for (let key of keys) {
+            if (books[key].author === author) {
+                matchingBooks.push(books[key]);
+            }
         }
-    }
+
+        if (matchingBooks.length > 0) {
+            resolve(matchingBooks);
+        } else {
+            reject("Author not found");
+        }
+
+
+    });
+
+    getBook
+        .then((book) => {
+            return res.send(book);
+        })
+        .catch((err) => {
+            return res.status(404).send(err);
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     //Write your code here
-    const keys = Object.keys(books);
     const title = req.params.title;
 
-    for (let key of keys) {
-        if (books[key].title === title) {
-            return res.send(books[key]);
+    const getBook = new Promise((resolve, reject) => {
+
+        const keys = Object.keys(books);
+        const matchingBooks = [];
+
+        for (let key of keys) {
+            if (books[key].title === title) {
+                matchingBooks.push(books[key]);
+            }
         }
-    }
+
+        if (matchingBooks.length > 0) {
+            resolve(matchingBooks);
+        } else {
+            reject("Title not found");
+        }
+
+
+    });
+
+    getBook
+        .then((book) => {
+            return res.send(book);
+        })
+        .catch((err) => {
+            return res.status(404).send(err);
+        });
 });
 
 //  Get book review
